4f9d9ed969a6cf20d2bda5d92bc00864a0ab20a6
阿循 <105115552+GANSOONLEE@users.noreply.github.com>
2023-03-12 09:31:22 +0000

    自定義錯誤信息 和 優化程式碼

--- src/module/TimeZone.ts
+++ src/module/TimeZone.ts
@@ -1,5 +1,6 @@
 
-export class CurrentTime {
+
+class CurrentTime {
     private date: Date;
 
     constructor() {
@@ -11,37 +12,35 @@ export class CurrentTime {
     }
 
     getDate(): string {
-        const year = this.date.getFullYear();
-        const month = this.date.getMonth() + 1;
-        const day = this.date.getDate();
+        const year = ('0' + this.date.getFullYear().toString()).slice(-4);
+        const month = ('0' + (this.date.getMonth() + 1).toString()).slice(-2);
+        const day = ('0' + this.date.getDate().toString()).slice(-2);
 
-        return `${year}:${month}:${day}`;
+        return `${year}-${month}-${day}`;
     }
 
     getTime(): string {
-        const hours = this.date.getHours();
-        const minutes = this.date.getMinutes();
+        const hours = ('0' + this.date.getHours().toString()).slice(-2);
+        const minutes = ('0' + this.date.getMinutes().toString()).slice(-2);
 
         return `${hours}:${minutes}`;
     }
 }
 
 const currentTime = new CurrentTime();
-console.log(currentTime.getTimezone());
-console.log(currentTime.getDate());
-console.log(currentTime.getTime());
+console.log("時區：" + currentTime.getTimezone() + "; UTC日期：" + currentTime.getDate() + "; UTC時間："  + currentTime.getTime());
 
-export class MyDateTime {
+class MyDateTime {
     private timeZone: string;
     private date: string;
     private time: string;
 
     constructor() {
         const now = new Date();
-        this.timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-        // this.timeZone = "Asia/Taipei" 測試用代碼
-        this.date = now.toLocaleDateString('en-US', { timeZone: this.timeZone });
-        this.time = now.toLocaleTimeString('en-US', { timeZone: this.timeZone });
+        // this.timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+        this.timeZone = "Asia/Taipei"
+        this.date = now.toLocaleDateString('en-US', { timeZone: this.timeZone});
+        this.time = now.toLocaleTimeString('en-US', { timeZone: this.timeZone, hour12: false});
     }
 
     getTimeZone(): string {
@@ -52,19 +51,20 @@ export class MyDateTime {
         const year = new Date(this.date).getFullYear();
         const month = new Date(this.date).getMonth() + 1;
         const day = new Date(this.date).getDate();
-        return `${year}:${month}:${day}`;
+
+        return `${('0'+year).slice(-4)}-${('0'+month).slice(-2)}-${('0'+day).slice(-2)}`;
     }
 
     getTime(): string {
         const timeParts = this.time.split(':');
-        const hours = parseInt(timeParts[0]);
-        const minutes = parseInt(timeParts[1].split(' ')[0]);
+        const hours = ('0' + parseInt(timeParts[0]).toString()).slice(-2);
+        const minutes = ('0' + parseInt(timeParts[1].split(' ')[0]).toString()).slice(-2);
+        // const hours = ('0' + this.date.getHours()).slice(-2);
+        // const minutes = ('0' + this.date.getMinutes()).slice(-2);
 
         return `${hours}:${minutes}`;
     }
 }
 
 const myDateTime = new MyDateTime();
-console.log(myDateTime.getTimeZone()); // "Asia/Taipei"
-console.log(myDateTime.getDate()); // "2023:3:12"
-console.log(myDateTime.getTime()); // "15:30"
+console.log("當前時區：" + myDateTime.getTimeZone() + "; 當前日期：" + myDateTime.getDate() + "; 當前時間：" + myDateTime.getTime()); // "Asia/Taipei"
---
+++ src/module/customLogger/CustomError.ts
@@ -0,0 +1,34 @@
+
+// ModuleError 類繼承自 Error 類
+class ModuleError extends Error {
+    public readonly module: string;
+    public readonly errorCode: string;
+
+    // 自定義錯誤信息模板
+    constructor(module: string, message: string, errorCode: string) {
+        super(`${module} ${message} (${errorCode})`);
+        this.module = module;
+        this.errorCode = errorCode;
+        this.name = 'Selective CSS';
+    }
+
+    // 將方法打包成一個公有函數 log()
+    public log() {
+        const [moduleName, message, code] = this.message.split(' ', 3);
+        console.error(`Error in module: ${moduleName} ${message} ${code}`);
+    }
+}
+
+// 測試程式碼
+try {
+
+    throw new ModuleError('TimeZone', '模塊發生錯誤！', '100');
+} catch (error) {
+    // 檢查錯誤是不是屬於 ModuleError 的，是的話調用 log() 方法
+    if (error instanceof ModuleError) {
+        error.log();
+    } else {
+        console.error(`Unknown error occurred: ${error.message}`);
+    }
+}
+
